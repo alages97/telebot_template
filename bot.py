02532e99f56a4b7f59377989acbfc77a3a10ac34
K Alages <e0309551@u.nus.edu>
2020-04-18 00:36:19 +0800

    first commit

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 import os
 from flask import Flask,request
 
-TOKEN = "1070333385:AAGipw6_OUFayEydbkbXBKKgrsrkA-0cQoU"
+TOKEN = "<Insert API Token from BotFather here>"
 bot = telebot.TeleBot(token=TOKEN)
 server = Flask(__name__)
 
@@ -18,20 +18,17 @@ def send_welcome(message):
 def echo_all(message):
 	bot.reply_to(message, message.text)
 
-
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://bot-template-alages.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://<insert heroku app name here>.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-
